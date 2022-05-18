9f5e5eb8fa13c3987e8a3065efa7ea1cfadd0772
Andres Mills Gallego <geneseewoodworking@gmail.com>
2022-05-18 01:00:39 -0700

    finished phase 2!

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { StatusBar } from 'expo-status-bar';
-import { Box, Heading, NativeBaseProvider, Center, Image } from 'native-base'
-import { StyleSheet, Text, View, Button, SafeAreaView } from 'react-native';
-import DateTimePicker, { DateTimePickerAndroid } from '@react-native-community/datetimepicker'
+import { Box, Heading, NativeBaseProvider, Center, Image, Button, Stack, Text, ScrollView, Link, HStack, Divider, Spinner } from 'native-base'
+import { LinearGradient } from 'expo-linear-gradient';
+import DateTimePicker from '@react-native-community/datetimepicker'
 
 export default function App() {
-  const [date, setDate] = useState(new Date(1598051730000));
+  const [date, setDate] = useState(new Date());
   const [mode, setMode] = useState('date');
   const [show, setShow] = useState(false);
 
@@ -14,9 +13,13 @@ export default function App() {
   const [stars, setStars] = useState(false);
 
   const handleChange = (event, selectedDate) => {
+    console.log(date)
     const currentDate = selectedDate;
     setShow(false);
     setDate(currentDate)
+    setTimeout(() => {
+      setStars(true)
+    }, 1500)
   }
 
   const showMode = (currentMode) => {
@@ -28,41 +31,106 @@ export default function App() {
     showMode('date');
   }
   const getImage = async () => {
+    console.log(date)
     const year = date.getFullYear();
     const month = date.getMonth();
     const day = date.getDate();
     let starryUrl = `https://api.nasa.gov/planetary/apod?api_key=d1IjMaxS0nc3bDZjuaP8ava66lSE0dnV5qAjrRco&date=${year}-${month}-${day}`
-    let response = await axios.get(starryUrl);
-    console.log(response.data);
-    setResponse(response.data);
-    setStars(true)
+    try {
+      let response = await axios.get(starryUrl);
+      console.log(response.data);
+      setResponse(response.data);
+    } catch (error) {
+      console.log('Response: ', response, 'Error: ', error.message)
+    }
   }
+  
+  // const handleReset = () => {
+  //   setStars(false)
+  //   showDatePicker()
+  // }
 
   useEffect(() => {
     getImage()
   }, [date])
 
+  const config = {
+    dependencies: {
+      "linear-gradient": LinearGradient
+    }
+  };
+
   return (
-    <NativeBaseProvider>
-      <Box>
-        <SafeAreaView>
-          <Center>
-            <Heading>Celestial Inspiration</Heading>
-            <Button onPress={showDatePicker} title='Pick A Date!' />
-            {show && (
-              <DateTimePicker
-                value={date}
-                mode={mode}
-                onChange={handleChange}
-              />
-            )}
-            {stars ?
-              <Image source={{ uri: response.url }} alt={response.title} size='xl' /> : null
-            }
-          </Center>
-        </SafeAreaView>
+
+    <NativeBaseProvider config={config}>
+      <Box
+        bg={{
+          linearGradient: {
+            colors: ["lightBlue.300", "violet.800"],
+            start: [0, 0],
+            end: [1, 0]
+          }
+        }}
+        safeAreaTop={20}
+        pt='25'
+        h='1000'
+      >
+        <Center>
+          {!stars ?
+            <Box mt='10'>
+              <Heading color='amber.200' >Celestial Calendar</Heading>
+              <Divider bgColor='danger.300' mb='5'/>
+              <Button onPress={showDatePicker} title='Pick A Date!' size='lg' my='10'>Pick A Date!</Button>
+              {show && (
+                <DateTimePicker
+                  value={date}
+                  mode={mode}
+                  onChange={handleChange}
+                  minimumDate={new Date(1995, 5, 16)}
+                  maximumDate={Date.now()}
+                />
+              )}
+                <Link href='https://github.com/AndresMillsGallego' _text={{color: 'pink.200', margin: 'auto'}} >My GitHub</Link>
+            
+            </Box> : null
+          }
+
+          {stars ?
+            <Box
+              p='5'
+            >
+              <ScrollView>
+                <Center>
+                  <Heading size='lg' bold color='cyan.200' pb='5'>{response.title}</Heading>
+                  <Divider bgColor='danger.300' mb='5'/>
+                  {response.url ? 
+                  <Image source={{ uri: response.url }} alt={response.title} size='500' rounded='lg' pb='5' />
+                  : <Spinner size='sm'/>
+                  }
+                </Center>
+                <Stack p='2' space={2}>
+                  <Divider bgColor='danger.300' mt='5' m='auto'/>
+                  <Text color='indigo.100' textAlign='center' fontSize='md'>{response.explanation}</Text>
+                  <Divider bgColor='danger.300' w='125' m='auto'/>
+                  {response.copyright?
+                  <Text color='purple.300' textAlign='center'>©{response.copyright}</Text>
+                  : null
+                  }
+                  <HStack justifyContent='center' space={4}>
+                    <Text textAlign='center' color='indigo.100'>{response.date}</Text>
+                    <Divider bgColor='danger.300' orientation='vertical' />
+                    <Link href='https://www.nasa.gov/' _text={{color: 'cyan.200'}}>Visit NASA</Link>
+                  </HStack>
+                  <Button onPress={() => setStars(false)} mb='20'>Search The Skies......</Button>
+                </Stack>
+              </ScrollView>
+            </Box>
+            : null
+          }
+        </Center>
       </Box >
     </NativeBaseProvider>
+
   );
 }
 
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Celestial 
+# Celestial
 
 React-Native application built by Andres Mills Gallego
 
@@ -8,17 +8,38 @@ React-Native application built by Andres Mills Gallego
 
 This app is very simple to use!  
 
-When the page first loads you simply follow the instructions: 
+When the page first loads you simply follow the instructions:
 
 - Click the **"Pick A Date"** button!
 - Select a date from the selector
 - ....profit!!!
 
-That is all there is to it.  You can select from any valid date and based on that date you will be bedazzled with NASA's "Astronamy Picture Of The Day", or **APOD** for short,  for that specific date along with a description. 
+That is all there is to it.  You can select from any valid date and based on that date you will be bedazzled with NASA's "Astronamy Picture Of The Day", or **APOD** for short,  for that specific date along with a description.
+
+## Details and Fun Facts
+
+For each unique date selected, an API call is made to NASA's database which returns some data on the `APOD` for that day.
+
+For this app we display the title, the image, the description and the date.
+The copyright is also rendered when applicable (this element is rendered conditionally, if it exists)
+
+On the initial page load, my github is linked if you would like to check it out!
+
+After selecting a date, viewing the spectacular image and reading all the interesting facts, you can begin your search again by hitting the button at the bottom of the page.  
+
+You will also see that NASA's webpage is linked there as well if you wanted to pay them a visit (hint...you should!)
+
+### Fun Fact
+
+Did you know that the `APOD` has been available since June 16 1995?  
+Taking that into consideration, we coded our date picker to only offer dates between then and whichever date the current day is 😁!
 
 ## Tools and Resources Used
 
 Tools, libraries and dependencies used for this application:
+
+1. Our imagination!! 🌠🤩
+2. Coffee ☕
 
 ## Dependcies
 
